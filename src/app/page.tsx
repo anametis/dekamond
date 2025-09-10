1825f91c603fc24923b0a5d5d5be08615dc1072e
Ali_S <alisafarpour.1997@gmail.com>
2025-09-11 03:08:19 +0330

    fix(auth): Update the bullion value on the root-page for correct redirecting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LoadingSpinner } from "@/components/ui/loading-spinner";
 import { useAuthGuard } from "@/hooks/use-auth-guard";
 
 export default function RootPage() {
-  const isReady = useAuthGuard(false, "/dashboard");
+  const isReady = useAuthGuard(true, "/dashboard");
 
   if (!isReady) return <LoadingSpinner />;
 
